7a20ea6ff05c5c65509e060a14c16b3326271632
riya <riyad17031998@gmail.com>
2020-04-27 15:14:41 +0530

    Improves handling of ssh error in sync-satellite logs
    Fixes #1609

--- server/bin/pbench-base.sh
+++ server/bin/pbench-base.sh
@@ -102,12 +102,17 @@ function log_finish {
 }
 
 function log_exit {
-    echo "${PROG}: ${1}" >&4
+    local _msg="${PROG}: ${1}"
+    if [[ -z "${3}" ]]; then
+	printf -- "%s\n" "${_msg}" >&4
+    else
+	printf -- "%s\n" "${_msg}" | tee -a "${3}" >&4
+    fi
     log_finish
     if [[ -z "${2}" ]]; then
-        exit 1
+	exit 1
     else
-        exit ${2}
+	exit ${2}
     fi
 }
 
